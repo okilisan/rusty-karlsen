d538c745a2a85bcd4914508f123f90f47b755a7e
okilisan <github@karlsencoin.com>
2025-03-19 22:38:17 +0100

    merging both PR-471, PR-439, PR-481, PR-510, PR-479 - some deep dive on PR-271

--- consensus/src/processes/transaction_validator/transaction_validator_populated.rs
+++ consensus/src/processes/transaction_validator/transaction_validator_populated.rs
@@ -1,7 +1,8 @@
 use crate::constants::{MAX_SOMPI, SEQUENCE_LOCK_TIME_DISABLED, SEQUENCE_LOCK_TIME_MASK};
-use karlsen_consensus_core::{hashing::sighash::SigHashReusedValues, tx::VerifiableTransaction};
+use karlsen_consensus_core::{hashing::sighash::SigHashReusedValues, tx::{TransactionInput, VerifiableTransaction},};
 use karlsen_core::warn;
 use karlsen_txscript::{get_sig_op_count, TxScriptEngine};
+use karlsen_txscript_errors::TxScriptError;
 
 use super::{
     errors::{TxResult, TxRuleError},
@@ -178,11 +179,19 @@ impl TransactionValidator {
                 &mut reused_values,
                 &self.sig_cache,
             )
-            .map_err(TxRuleError::SignatureInvalid)?;
-            engine.execute().map_err(TxRuleError::SignatureInvalid)?;
+            .map_err(|err| map_script_err(err, input))?;
+            engine.execute().map_err(|err| map_script_err(err, input))?;
         }
 
         Ok(())
+    }
+}
+
+fn map_script_err(script_err: TxScriptError, input: &TransactionInput) -> TxRuleError {
+    if input.signature_script.is_empty() {
+        TxRuleError::SignatureEmpty(script_err)
+    } else {
+        TxRuleError::SignatureInvalid(script_err)
     }
 }
 
